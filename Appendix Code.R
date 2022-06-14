1e7aacee669563c3cc1ea1d0eb4c068ed75dd962
Vale-LaMo <100314333+Vale-LaMo@users.noreply.github.com>
2022-06-14 18:30:51 +0200

    Prove sul codice generale

--- Appendix Code.R
+++ Appendix Code.R
@@ -2,6 +2,10 @@
 #### By simulating a plausible set of forward distances
 
 library(mvtnorm)
+library(tidyverse)
+# if(!"devtools" %in% rownames(installed.packages())) {install.packages("devtools")}
+# devtools::install_github('david-borchers/LT2D')
+library(LT2D)
 
 #### Load 2D distance functions
 source("com_hfunctions.R")
@@ -9,29 +13,35 @@ source("com_pifunctions.R")
 source("com_likelihoodutilities.R")
 
 #### Load dataset
-data <- read.csv("montiCORRIGE20122013.csv", sep=";")
-colnames(data)[11] = "x" ## rename perpendicular distance to x
-data$heure_deb = as.POSIXct(as.character(data$heure_deb),format = "%d/%m/%Y %H:%M:%S")
-data$heure_fin = as.POSIXct(as.character(data$heure_fin),format = "%d/%m/%Y %H:%M:%S")
-data$heure_obs = as.POSIXct(as.character(data$heure_obs),format = "%d/%m/%Y %H:%M:%S")
+data <- read.csv("monticola_FINAL_20122013.csv", sep=",")
+data %>% 
+  rename(x = distance) -> data ## rename perpendicular distance to x
+data$transect_start_time = as.POSIXct(as.character(data$transect_start_time),format = "%d/%m/%Y %H:%M:%S")
+data$transect_end_time = as.POSIXct(as.character(data$transect_end_time),format = "%d/%m/%Y %H:%M:%S")
+data$obs_time = as.POSIXct(as.character(data$obs_time),format = "%d/%m/%Y %H:%M:%S")
 
 #### Transect lengths 
 transect.names = c("A","I","J","K","L")
 transect.lengths = c(8, 6, 5.8, 6, 5.4)   # A I J K L
 names(transect.lengths) =  transect.names
-data = data[data$transect %in% transect.names,]
+data = data[data$transect_id %in% transect.names,]
 dim(data)
-table(data$transect, as.POSIXlt(data$heure_obs)$year)
+table(data$transect_id, as.POSIXlt(data$obs_time)$year)
 
 #### Generate forward distance data
 ####    (scenario letters correspond to Fig. 3 in the main text)
+scenario = "a"
 if (scenario == "a")
-  data$y = pmax(0.01, data$x + rnorm(length(data$x), -1+(data$contact=="Crotte"), 1))
+  data$y = pmax(0.01, data$x + rnorm(length(data$x), -1+(data$obs_type=="Crotte"), 1))
+# nel primo scenario la distanza yi è all'incirca dello stesso ordine di grandezza di xi, con qualche variabilità gaussiana
 if (scenario == "b") {
   mx=max(data$x)
-  data$y = pmax(0.01, (mx/2)*exp(-data$x/(mx/2)) + rnorm(length(data$x), -1+(data$contact=="Crotte"), 1))
+  data$y = pmax(0.01, (mx/2)*exp(-data$x/(mx/2)) + rnorm(length(data$x), -1+(data$obs_type=="Crotte"), 1))
+# correlazione negativa tra yi e xi, più sei vicino più si allontanano, più grande yi più piccolo xi
+  # la riga sopra è un modo per creare una correlazione negativa basata sull'esponenziale
 }
 if (scenario == "c") {
+  # nel terzo scenario yi e xi sono indipendenti
   b = c(0.2,0.2)
   x<-runif(length(data$x), px(13-0.01,rev(b),h.RE,13,nint=100),1)
   f <- function(x, u) px(x,rev(b),h.RE,13,nint=100) - u
@@ -41,17 +51,18 @@ if (scenario == "c") {
 
 
 #### Divide into year- and cue-specific datasets
-montiVU2012 = data[as.POSIXlt(data$heure_obs)$year==112 & data$contact=="Vu" & data$type_obs=="Nocturne",]
-montiCR2012 = data[as.POSIXlt(data$heure_obs)$year==112 & data$contact=="Crotte" & data$type_obs=="Diurne",]
-montiVU2013 = data[as.POSIXlt(data$heure_obs)$year==113 & data$contact=="Vu" & data$type_obs=="Nocturne",]
-montiCR2013 = data[as.POSIXlt(data$heure_obs)$year==113 & data$contact=="Crotte" & data$type_obs=="Diurne",]
+# crea 4 dataset dividendo in base al tipo di dato e all'anno
+montiVU2012 = data[as.POSIXlt(data$obs_time)$year==112 & data$obs_type=="Vu" & data$obs_period=="Nocturne",]
+montiCR2012 = data[as.POSIXlt(data$obs_time)$year==112 & data$obs_type=="Crotte" & data$obs_period=="Diurne",]
+montiVU2013 = data[as.POSIXlt(data$obs_time)$year==113 & data$obs_type=="Vu" & data$obs_period=="Nocturne",]
+montiCR2013 = data[as.POSIXlt(data$obs_time)$year==113 & data$obs_type=="Crotte" & data$obs_period=="Diurne",]
 unlist(lapply(list(montiVU2012,montiCR2012,montiVU2013,montiCR2013), function(x) dim(x)[1]))
 
 
 #### Fit 2D distance sampling model using multiple initial value to avoid local minima in the deviance
  y = c(montiVU2012$y , montiVU2013$y)
  x = c(montiVU2012$x , montiVU2013$x)
- hr = h.RE
+ hr = h.RE # h.yTRE non è compatibile con pi.sigmoI
  pi.x = pi.sigmo
  ystart = ceiling(max(y))
  w = ceiling(max(x))
@@ -61,8 +72,10 @@ unlist(lapply(list(montiVU2012,montiCR2012,montiVU2013,montiCR2013), function(x)
  FIT=list(); dev=NULL
  for (m in 1:10) {
    pars = rnorm(4, c(0.25,0.25,-4,-1), 3)
+   # pars = rnorm(4, c(0.25,0.25,4,1), 3)
    tmp0 <- tryCatch.W.E (
-     fityx(y,x,pars[1:length.b],hr,ystart,pi.x,pars[(length.b+1):length(pars)],w,control=list(),hessian=TRUE,corrFlag=0.7,debug=FALSE)
+     fityx(y,x,pars[1:length.b],
+           hr,ystart,pi.x,pars[(length.b+1):length(pars)],w,control=list(),hessian=TRUE,corrFlag=0.7,debug=FALSE)
    )
    fit = NA
    if(! "error" %in% class(tmp0$value)) {
@@ -78,6 +91,24 @@ unlist(lapply(list(montiVU2012,montiCR2012,montiVU2013,montiCR2013), function(x)
  tmp1 <- tryCatch.W.E (boot(fitVU))
  if(! "error" %in% class(tmp1$value))  tabVU=tmp1$value
  }
+ 
+ # vedere https://github.com/david-borchers/LT2D/blob/master/inst/FitsForPaper.r
+ # linea nera sigmoide = distribuzione reale animali
+ # linea grigia = detection osservata
+ # linea tratteggiata = detection corretta tenendo conto della risposta comportamentale
+ plotfit.x(x[x<=w],fitVU,nclass=20);rug(x[x<=w])
+ GoFx(fitVU,plot=TRUE)$pvals
+ #plotfit.y(y[x<=w],x,fit.n,nclass=20);rug(x=y[x<=w])
+ plotfit.smoothfy(fitVU,nclass=32);rug(x=y[x<=w])
+ GoFy(fitVU,plot=TRUE)$pvals
+ #EHSW:
+ phatInterval(fitVU)
+ phatInterval(fitVU)*w
+ # p(0):
+ p0.n=1-Sy(0,0,ystart,fitVU$b,h1);p0.n
+ plotfit.smoothfy(fitVU,xmax=0.004)
+ 
+ 
  
  y = c(montiCR2012$y , montiCR2013$y) 
  x = c(montiCR2012$x , montiCR2013$x)
@@ -114,4 +145,12 @@ tabCR[2,]*(fitVU$w/fitCR$w)/tabVU[2,]
 ### Population density
 tabVU[2,]/sum(transect.lengths)/(fitVU$w/1000) /2 ## divided by two to get the average over 2 years
 ### Fit details (parameters values, fitted detection and avoidance functions) are in the objects fitVU and fitCR for live and dung data respectively
+tabVU[2,]/sum(transect.lengths)/(fitVU$w/1000) /2 -> res1
+
+
+res1
+names(fitVU)
+class(fitVU$hr)
+
+
 
